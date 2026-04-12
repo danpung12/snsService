5667689f120bb609b8cde78e5c9f78aef2b02339
이은총 <aass6863@naver.com>
2026-04-12 21:22:27 +0900

    refactor: Express에서 NestJS로 프로젝트 초기 환경 구축
    기존 Node/Express 기반 프로젝트를 NestJS 프레임워크로 마이그레이션하기 위한 기본 뼈대 구축
    
    main.ts, app.module.ts 등 NestJS 진입점 및 전역 모듈 구성
    
    데이터베이스 연동을 위한 Prisma 스키마 및 설정 파일 세팅
    
    프론트엔드 통신을 위한 CORS 및 Credentials 옵션 설정 추가
    
    https://velog.io/@aass6863/Nest-0-기초

---
+++ leesns-backend/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // "이 주소(프론트)에서 오는 요청은 막지 마!"
+    credentials: true, // "나중에 쿠키나 인증 헤더 같은 것도 통과시켜 줘!"
+  });
+  await app.listen(process.env.PORT ?? 4000);
+}
+
+bootstrap();
